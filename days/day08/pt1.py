3768e11f6a89a8d6fdf48773f6940b0956ecf0d7
jovan.radivojsa <jovan.radivojsa@gmail.com>
2025-12-08 09:29:19 +0100

    Day 8 pt1 done

--- days/day08/pt1.py
+++ days/day08/pt1.py
@@ -2,6 +2,7 @@
 """Advent of Code 2025 - Day 08 Part 1"""
 
 import os
+import heapq
 
 def load_input():
     """Load and return input lines from input1.txt"""
@@ -17,11 +18,75 @@ def load_input():
         print("Please create input1.txt with your puzzle input.")
         return
 
-def solve():
-    """Solve Part 1"""
+def load_lines():
+    lines = []
     for line in load_input():
-        # TODO: Process each line
-        print(line)
+        str_line = line.split(",")
+        lines.append([int(x) for x in str_line])
+    return lines
+
+# Union find with path compression
+class UnionFind:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+        self.rank = [1] * size
+        self.size = [1] * size
+    
+    def find(self, p):
+        if self.parent[p] != p:
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+    
+    def union(self, p, q):
+        rootP = self.find(p)
+        rootQ = self.find(q)
+        if rootP != rootQ:
+            if self.rank[rootP] > self.rank[rootQ]:
+                self.parent[rootQ] = rootP
+                self.size[rootP] += self.size[rootQ]
+            elif self.rank[rootP] < self.rank[rootQ]:
+                self.parent[rootP] = rootQ
+                self.size[rootQ] += self.size[rootP]
+            else:
+                self.parent[rootQ] = rootP
+                self.rank[rootP] += 1
+                self.size[rootP] += self.size[rootQ]
+    
+    def get_component_sizes(self):
+        sizes = [self.size[i] for i in range(len(self.size)) if self.parent[i] == i]
+        return sizes
+    
+
+def solve():
+    num_to_connect = 1000
+    mul_top_count = 3
+    lines = load_lines()
+    total = 0
+    heap = []
+    # Precompte distances and place them in a heap
+    for i in range(len(lines)):
+        x1, y1, z1 = lines[i]
+        for j in range(i + 1, len(lines)):
+            x2, y2, z2 = lines[j]
+            dist = abs(x1 - x2) * abs(x1 - x2) + abs(y1 - y2) * abs(y1 - y2) + abs(z1 - z2) * abs(z1 - z2)
+            heapq.heappush(heap, (dist, i, j))
+    
+    heapq.heapify(heap)
+    uf = UnionFind(len(lines))
+    for _ in range(num_to_connect):
+        dist, i, j = heapq.heappop(heap)
+        uf.union(i, j)
+    
+    sorted_circuit_sizes = uf.get_component_sizes()
+    sorted_circuit_sizes.sort(reverse=True)
+    print(sorted_circuit_sizes)
+    
+    total = 1
+    for i in range(mul_top_count):
+        total *= sorted_circuit_sizes[i]
+    print(total)
+    
+        
 
 if __name__ == "__main__":
     solve()
